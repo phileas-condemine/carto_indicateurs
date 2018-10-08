78c4285d691d246c2b99e96a3c2de68340d3f822
phileas-condemine <phileas.condemine@sante.gouv.fr>
2018-10-08 13:24:52 +0200

    ajout d'un lien vers github en footer

--- ui.R
+++ ui.R
@@ -5,11 +5,20 @@ fluidPage(
   fluidRow(align="center",
           div(class="tagbar",
               selectInput(inputId="tag",label = "Recherche par tags",choices = tag_names,multiple=T)
-  )),
+              )
+,
   
   fluidRow(div(class="resultats",
       dataTableOutput("DT_to_render")
       )
+  ),
+  div(class="footer",
+    a(
+      img(src="GitHub-Mark.png",width="50px",heigth="50px")
+      ,href="https://github.com/phileas-condemine/carto_indicateurs"
+      ,target="_blank"
+    )
+  )
   )
   
 
